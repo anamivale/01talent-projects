2cf362599006619cf61bd440c4686a7f1f50c47a
vmuhembe <muhembelev@gmail.com>
2024-05-10 02:28:05 +0300

    test

--- ascii-art/main.go
+++ ascii-art/main.go
@@ -1,9 +1,71 @@
 package main
 
-import "os"
-
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
 	testFile, _ := os.ReadFile("standard.txt")
-	
+	inputString := os.Args[1]
+
+	inputFile := strings.Split(string(testFile), "\n")
+
+	output := AsciiArt(inputString, inputFile)
+	fmt.Print(output)
+}
+
+func AsciiArt(input string, inputFile []string) string {
+	var result strings.Builder
+	var newLinesOnly strings.Builder
+	sepInputString := strings.Split(input, "\\n")
+
+	newLines := OnlyNewLines(sepInputString)
+	if newLines != "false" {
+		newLinesOnly.WriteString(newLines)
+		return newLinesOnly.String()
+
+	}
+
+	for _, words := range sepInputString {
+		if words == "" {
+			result.WriteString("\n")
+		} else {
+			for i := 0; i < len(words); {
+				for j := 0; j < 8; {
+					start := (int(words[i]-32) * 9) + 1
+					result.WriteString(inputFile[start+j])
+					i++
+					if i == len(words) {
+						if j == 7 {
+							result.WriteString("\n")
+							break
+						}
+						result.WriteString("\n")
+						j++
+						i = 0
+
+					}
+				}
+			}
+		}
+	}
+	return result.String()
+}
+
+func OnlyNewLines(sepInputString []string) string {
+	empty := ""
+	for i, words := range sepInputString {
+		if words != "" {
+			return "false"
+		}
+		if i == 0 && words == "" {
+			continue
+		}
+		empty += "\n"
+
+	}
+
+	return empty
 }
---
+++ ascii-art/main_test.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestAsciiArt(t *testing.T) {
+	file, _ := os.ReadFile("standard.txt")
+	inputFile := strings.Split(string(file), "\n")
+	test1, _ := os.ReadFile("test1.txt")
+	test2, _ := os.ReadFile("test2.txt")
+	type args struct {
+		input     string
+		inputFile []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			"words with new line at the end",
+			args{
+				input:     "Hello\n",
+				inputFile: inputFile,
+			},
+			string(test1),
+		},
+		{
+			"words with two new line in the middle",
+			args{
+				input:     "Hello\n\nThere",
+				inputFile: inputFile,
+			},
+			string(test2),
+		},
+		{
+			"empty",
+			args{
+				input:     "",
+				inputFile: inputFile,
+			},
+			"",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := AsciiArt(tt.args.input, tt.args.inputFile); got != tt.want {
+				t.Errorf("AsciiArt() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+
+func TestOnlyNewLines(t *testing.T) {
+	tests := []struct {
+		name           string
+		sepInputString []string
+		want           string
+	}{
+		{
+			"Lines with words",
+			[]string{"", "welcome", ""},
+			"false",
+		},
+		{
+			"only lines",
+			[]string{"", "", ""},
+			"\n\n",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := OnlyNewLines(tt.sepInputString); got != tt.want {
+				t.Errorf("OnlyNewLines() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
