eb8fccd6b8945c0527d2b3727b72db75ecf82c1d
vmuhembe <vmuhembe@gmail.com>
2024-07-21 16:49:43 +0300

    printing the ascii using the logic of maps

--- ascii-art-output/functions/printascii.go
+++ ascii-art-output/functions/printascii.go
@@ -1,5 +1,58 @@
 package functions
 
-func Mapping(fileContent []string)  {
+import (
+	"strings"
+)
+
+func Mapping(fileContent []string) map[rune][]string {
+	mapBanner := make(map[rune][]string)
+	runs := 32
+	for i, str := range fileContent {
+		if str == "" {
+			if i == 0 {
+				continue
+			}
+			runs++
+			continue
+		}
+		mapBanner[rune(runs)] = append(mapBanner[rune(runs)], str)
+
+	}
+	return mapBanner
+}
+
+func PrintAscii(mapBanner map[rune][]string, userInput string) string {
+	var outputStr strings.Builder
+	userInput = strings.ReplaceAll(userInput, "\n", "\\n")
+	// userInput = strings.ReplaceAll(userInput, "\t", "\\n")
+	validStr := strings.Split(userInput, "\\n")
+
+	for i, str := range validStr {
+		if str == "" {
+			if i==0{
+				continue
+			}
+			outputStr.WriteString("\n")
+		} else {
+			 output := make([]string, 8)
+			for _, char := range str {
+				if value, ok := mapBanner[char]; ok{
+				for i:= 0; i<8; i++ {
+					output[i] += value[i]
+				}
+
+				} else {
+					return "!ok"
+				}
+			}
+			for i:= 0; i<8; i++ {
+				outputStr.WriteString(output[i]+"\n")
+			}
+			
+
+		}
+	}
+
+return outputStr.String()
 
 }
--- ascii-art-output/main.go
+++ ascii-art-output/main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 	file "output/functions"
 )
 
 func main() {
-	x := file.ValidateBanner("shadow.txt")
+	var outputFile string
+	flag.StringVar(&outputFile, "output", "standard.txt", "banner file")
+	flag.Parse()
+	fmt.Println(outputFile)
+	if len(os.Args) != 2 || len(os.Args) !=4 {
+		fmt.Println("Usage: go run . [OPTION] [STRING] [BANNER]\n\nEX: go run . --output=<fileName.txt> something standard")
+	}
+	x := file.ValidateBanner("standard.txt")
 	if x == nil {
 		fmt.Println("file not valid or empty")
 		return
 	}
-	fmt.Println(x[20])
+	y := file.Mapping(x)
+	z := file.PrintAscii(y, "val")
+	if z == "!ok"{
+		fmt.Println("character not within the range (32-126)")
+	}
+
+	fmt.Print(z)
 }
