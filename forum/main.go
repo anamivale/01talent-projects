187852eaf0479ff92468f169e0f4d2717d005f00
vmuhembe <muhembelev@gmail.com>
2025-01-20 10:29:08 +0300

    feat: add handlers to the main page

--- forum/main.go
+++ forum/main.go
@@ -1,4 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/anamivale/01talent-projects.git/handlers"
+	"github.com/anamivale/01talent-projects.git/middlewares"
+)
+
 func main() {
+	middlewares.DbConn()
+
+	middlewares.CreateUsersTable()
+	middlewares.CreatePostsTable()
+	middlewares.CreateCommentTable()
+
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/register", handlers.Register)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/login", handlers.Login)
+
+	fmt.Println("Server running at http://localhost:3000")
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		fmt.Println("Server failed to start:", err.Error())
+	}
 }
